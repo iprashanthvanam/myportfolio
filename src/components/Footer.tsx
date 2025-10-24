5503ec4545cf29e4d8b5cedf7f602ad9f4c1972a
iprashanthvanam <prashanthvanamnetha@gmail.com>
2025-10-24 23:34:21 +0530

    Update footer links to new user profiles

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,8 +9,8 @@ function Footer() {
   return (
     <footer>
       <div>
-        <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-        <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+        <a href="https://github.com/iprashanthvanam/" target="_blank" rel="noreferrer"><GitHubIcon/></a>
+        <a href="https://www.linkedin.com/in/iprashanthvanam/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
         <a href="https://leetcode.com/u/iprashanthvanam/" target="_blank" rel="noreferrer"><LeetcodeIcon size={24} /></a>
       </div>
       <p>A portfolio designed & built by <a href="https://github.com/yujisatojr/react-portfolio-template" target="_blank" rel="noreferrer">Yuji Sato</a> with 💜</p>
